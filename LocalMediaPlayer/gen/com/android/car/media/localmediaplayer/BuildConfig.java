c1796a26b417f96dc5d2351765cb84902fdc1951
sunxiaolin2016 <sun.xiaolin@icloud.com>
2019-10-02 19:53:02 +0800

    first commit

---
+++ LocalMediaPlayer/gen/com/android/car/media/localmediaplayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.car.media.localmediaplayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
